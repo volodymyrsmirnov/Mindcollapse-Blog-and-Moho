e7fb9f6e13e3b37d342e6ee26f9e39d73d192aff
Vladimir Smirnov <mail@mindcollapse.com>
2012-11-06 10:00:13 +0200

    add required javascript

--- static/blog/main.js
+++ static/blog/main.js
@@ -37,6 +37,9 @@ $(function(){
 		window.open ($(this).attr('href'), $(this).attr('title'),"location=0,status=0,scrollbars=0,width=640,height=480");
 	})
 
+	$("img.lazy").lazyload({
+		effect : "fadeIn"
+ 	});
 
 	if (!ismobile) {
 
