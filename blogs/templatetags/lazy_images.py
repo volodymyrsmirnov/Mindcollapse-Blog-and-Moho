3bd32354178d1b2b2afa790c5e343780ab406124
Vladimir Smirnov <mail@mindcollapse.com>
2012-11-06 09:34:55 +0200

    lazy load template

--- blogs/templatetags/lazy_images.py
+++ blogs/templatetags/lazy_images.py
@@ -1,14 +1,12 @@
-
 from django import template
 
 register = template.Library()
 
+@register.filter(is_safe=True)
 def lazy_images(value):
-	for match in re.finditer('<img.*?class="lazy".*?>', value):
-		replace_from = match.group(0)
-		replace_to = match.group(0).replace('src=', 'src="/static/blog/grey.gif" data-original=')
-		value = value.replace(replace_from, replace_to)
+    for match in re.finditer('<img.*?class="lazy".*?>', value):
+        replace_from = match.group(0)
+        replace_to = match.group(0).replace('src=', 'src="/static/blog/grey.gif" data-original=')
+        value = value.replace(replace_from, replace_to)
 
-	return value
-
-register.filter('lazy_images', lazy_images)
+    return value
