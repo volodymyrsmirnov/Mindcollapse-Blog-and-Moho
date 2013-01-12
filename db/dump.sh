7fced1b2919714782e1cb42eaac473bac14b6c3f
Vladimir Smirnov <vladimir@smirnov.im>
2013-01-12 04:10:01 -0600

    Database dump Sat Jan 12 04:10:01 CST 2013

--- db/dump.sh
+++ db/dump.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /home/smirnov/www/mindcollapse.com/db
-mysqldump -usmirnov_dump -pblogdump --skip-comments --skip-extended-insert --compact --lock-tables=false --ignore-table=smirnov_mindcollapse.auth_user --ignore-table=smirnov_mindcollapse.django_session smirnov_mindcollapse > db.sql
+mysqldump -usmirnov_dump -pblogdump --skip-comments --skip-extended-insert --compact --lock-tables=false --ignore-table=smirnov_mindcollapse.django_admin_log --ignore-table=smirnov_mindcollapse.auth_user --ignore-table=smirnov_mindcollapse.django_session 
+smirnov_mindcollapse > db.sql
 git commit -a -m "Database dump $(date)"
 git push
