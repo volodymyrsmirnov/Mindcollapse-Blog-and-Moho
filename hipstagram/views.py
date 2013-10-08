0f2d8ee7761112c3c7526a4264751c6110ae80fa
Vladimir Smirnov <mail@mindcollapse.com>
2013-10-08 17:20:20 +0300

    add cache to the instagram fetching

--- hipstagram/views.py
+++ hipstagram/views.py
@@ -3,10 +3,12 @@ from instagram.client import InstagramAPI
 from django.conf import settings
 from django.http import HttpResponsePermanentRedirect, HttpResponse
 from django.shortcuts import render_to_response
+from django.views.decorators.cache import cache_page
 
 def index(request):
 	return HttpResponsePermanentRedirect(settings.INSTAGRAM_URL)
 
+@cache_page(60 * 15)
 def rss(request):
 	api = InstagramAPI(access_token=settings.INSTAGRAM_TOKEN)
 
