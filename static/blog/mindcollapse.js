728354a54f9f40c34c3bc9b337bcc8d0905b52a4
Vladimir Smirnov <mail@mindcollapse.com>
2013-10-16 17:01:31 +0300

    remove responsive img from js to css

--- static/blog/mindcollapse.js
+++ static/blog/mindcollapse.js
@@ -1,6 +1,4 @@
 $(function(){
-	$(".content img").addClass("img-responsive");
-
 	$(".social a").click(function(e){
 		e.preventDefault();
 		window.open ($(this).attr("href"), $(this).attr("title"), "location=0,status=0,scrollbars=0,width=640,height=480");
