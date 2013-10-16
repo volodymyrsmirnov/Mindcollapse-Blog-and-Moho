6eb2ec00934ce96ea0cfab193dd33178149f23e4
Vladimir Smirnov <mail@mindcollapse.com>
2013-10-16 11:03:56 +0300

    finally add working crossplatform hyphenation solution

--- blogs/models.py
+++ blogs/models.py
@@ -1,8 +1,14 @@
 from django.core.urlresolvers import reverse
+from softhyphen.html import hyphenate
 
 from django.db import models
+from django.db.models.signals import post_save
 
-class Post (models.Model):
+from django.core.cache import cache
+
+from blogs.helpers import make_template_fragment_key
+
+class Post(models.Model):
 	createdAt = models.DateTimeField(verbose_name="Publication date")
 	visible = models.BooleanField(default=False, verbose_name="Published", db_index=True)
 	title = models.CharField(max_length=128)
@@ -17,7 +23,7 @@ class Post (models.Model):
 			return reverse('blogs.views.id', args=[self.id])
 
 	def text_cleaned(self):
-		return self.text.replace("&nbsp;", " ")
+		return hyphenate(self.text.replace("&nbsp;", " "), language="ru-ru")
 
 	def __unicode__(self):
 		return self.title	
@@ -28,3 +34,13 @@ class Tag(models.Model):
 	
 	def __unicode__(self):
 		return self.name.capitalize()
+
+def clear_template_cache(sender, **kwargs):
+	post = kwargs.get("instance")
+
+	key = make_template_fragment_key("posts", [post.id])
+
+	if key:
+		cache.delete(key)
+
+post_save.connect(clear_template_cache, sender=Post)
